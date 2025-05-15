38196cf68fe758e8b83614401a773b46cf8d4fc9
houkinki <41355515+houkinki@users.noreply.github.com>
2025-05-15 10:40:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  cookie: 'csrf_session_id=9ef08de6f7704d38a43c9d825881068d; _tea_utm_cache_2608=undefined; __tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227497904080607282738%2522%252C%2522user_unique_id%2522%253A%25227497904080607282738%2522%252C%2522timestamp%2522%253A1745741848367%257D; passport_csrf_token=157fd19c5e583dc149cf00fef5413584; passport_csrf_token_default=157fd19c5e583dc149cf00fef5413584; is_staff_user=false; _ga=GA1.2.1718178484.1745899335; _gid=GA1.2.216124038.1745899335; _ga_S695FMNGPJ=GS1.2.1745899335.1.0.1745899335.60.0.0; odin_tt=c6c93d5de0498772bbc4c3fb0cee20cdcf1f48808c7231086e6d2b25341a99eb294b8e87c3875bb63bef5c5d891eba3d28b0f4b28a953006372e286e0a9ea760; passport_auth_status=9b982a8ea7292d09b46921f13d92cffc%2Ce46bd2de8eb19602ea4e90d240620269; passport_auth_status_ss=9b982a8ea7292d09b46921f13d92cffc%2Ce46bd2de8eb19602ea4e90d240620269; n_mh=j69Sf2cChiKESq69IjbYZCZROAZL8_9yU7MvNA_NLMA; sid_guard=dcf53a92fdfc5985a4ea317c826a57cb%7C1745902782%7C31536000%7CWed%2C+29-Apr-2026+04%3A59%3A42+GMT; uid_tt=7cc1046b5ae25f9426e7ea15fc75ba84; uid_tt_ss=7cc1046b5ae25f9426e7ea15fc75ba84; sid_tt=dcf53a92fdfc5985a4ea317c826a57cb; sessionid=dcf53a92fdfc5985a4ea317c826a57cb; sessionid_ss=dcf53a92fdfc5985a4ea317c826a57cb; sid_ucp_v1=1.0.0-KGE5ZTEyYzVlZTQ4NDdiYmVkMzE1OTc3Yzg1ZTc1MjM5YTlkMWE4NzAKFwj5z9Dzjaz8AxC-ucHABhiwFDgCQO8HGgJsZiIgZGNmNTNhOTJmZGZjNTk4NWE0ZWEzMTdjODI2YTU3Y2I; ssid_ucp_v1=1.0.0-KGE5ZTEyYzVlZTQ4NDdiYmVkMzE1OTc3Yzg1ZTc1MjM5YTlkMWE4NzAKFwj5z9Dzjaz8AxC-ucHABhiwFDgCQO8HGgJsZiIgZGNmNTNhOTJmZGZjNTk4NWE0ZWEzMTdjODI2YTU3Y2I',
+  cookie: '_tea_utm_cache_2608=undefined; __tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227497904080607282738%2522%252C%2522user_unique_id%2522%253A%25227497904080607282738%2522%252C%2522timestamp%2522%253A1745741848367%257D; passport_csrf_token=157fd19c5e583dc149cf00fef5413584; passport_csrf_token_default=157fd19c5e583dc149cf00fef5413584; is_staff_user=false; _ga=GA1.2.1718178484.1745899335; _ga_S695FMNGPJ=GS1.2.1745899335.1.0.1745899335.60.0.0; odin_tt=c6c93d5de0498772bbc4c3fb0cee20cdcf1f48808c7231086e6d2b25341a99eb294b8e87c3875bb63bef5c5d891eba3d28b0f4b28a953006372e286e0a9ea760; passport_auth_status=9b982a8ea7292d09b46921f13d92cffc%2Ce46bd2de8eb19602ea4e90d240620269; passport_auth_status_ss=9b982a8ea7292d09b46921f13d92cffc%2Ce46bd2de8eb19602ea4e90d240620269; n_mh=j69Sf2cChiKESq69IjbYZCZROAZL8_9yU7MvNA_NLMA; sid_guard=78694b5b42472aed87a2d924d4d7e6b9%7C1745909151%7C31536000%7CWed%2C+29-Apr-2026+06%3A45%3A51+GMT; uid_tt=5d7635004f26097d9d43e686a6fe2caf; uid_tt_ss=5d7635004f26097d9d43e686a6fe2caf; sid_tt=78694b5b42472aed87a2d924d4d7e6b9; sessionid=78694b5b42472aed87a2d924d4d7e6b9; sessionid_ss=78694b5b42472aed87a2d924d4d7e6b9; sid_ucp_v1=1.0.0-KGZmOTRjMzkxMjBhZjgwODcwOTMwOWI4NmEzZTczMGQzNjhhODdjNDEKFwj5z9Dzjaz8AxCf68HABhiwFDgCQO8HGgJsZiIgNzg2OTRiNWI0MjQ3MmFlZDg3YTJkOTI0ZDRkN2U2Yjk; ssid_ucp_v1=1.0.0-KGZmOTRjMzkxMjBhZjgwODcwOTMwOWI4NmEzZTczMGQzNjhhODdjNDEKFwj5z9Dzjaz8AxCf68HABhiwFDgCQO8HGgJsZiIgNzg2OTRiNWI0MjQ3MmFlZDg3YTJkOTI0ZDRkN2U2Yjk;',
   PUSH_PLUS_TOKEN: '',
   aid: '2608',
   uuid: '7497904080607282738',
-  msToken:'hwpd0JJ-ABL-DgaB1iDoyRURFFJi9jModANQ0iCU6W1ko0zCQ77ViWAGub0yybQB6OnVkb18gleQH8LacUKs4ClDnssGUlDy_jlQhvH5j3Z2zWb6k1g6y61KBOXSLOqZ2T8%3D',
-  a_bogus:'YjldvcZqMsm1kDVv9wDz9nimGKY0YW-6gZEzzAMeY0of',
+  msToken:'0r4-2dpoip966yv8G44eqUK0NEA7Bi6ezVvzKjIEAVYUIHfGod3wlvbiAkO7-aUSQZORHkEiuhXx_H6byevuOqrybSsA1hCkE2a16gCwUu-dfq8TvS9ZMVwLOXA1-28bLI8%3D',
+  a_bogus:'YfsQDchXMsm1Bh33ghDz976EynR0YW-zgZENknLroUog',
   _signature: '',
   spider:0,
   DING_TALK_TOKEN: '', // 钉钉webhook参考（很简单）：https://open.dingtalk.com/document/group/custom-robot-access
-  uid: '1192328696173751', // *自动玩游戏需要此参数，在掘金首页打开控制台输入这行代码`window.__NUXT__.state.auth.user.id`就可以得到
-  lottery_history_id:"7187579039605850124",
+  uid: '282990548094505', // *自动玩游戏需要此参数，在掘金首页打开控制台输入这行代码`window.__NUXT__.state.auth.user.id`就可以得到
+  lottery_history_id:"7498573645465404937",
   emailInfo:{
     "user": "sunsgnes@163.com",
     "from": "sunsgnes@163.com",
